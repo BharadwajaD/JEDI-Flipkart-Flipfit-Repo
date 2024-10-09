394932538f733226d2f0a20aacb34e4c49221da8
arpit.shrivastava <f20190818@pilani.bits-pilani.ac.in>
2024-10-09 12:08:47 +0530

    updated interface and buisness to gym

--- JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/business/Interface/GymCentre.java
+++ JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/business/Interface/GymCentre.java
@@ -3,5 +3,5 @@ package com.flipfit.business.Interface;
 import com.flipfit.bean.Gym;
 
 public interface GymCentre {
-    public Gym updateGymCentre(int gymId, String gymName, int capacity, boolean listed, int userId);
+    public void updateGymCentre(int gymId, String gymName, int capacity, boolean listed, int userId);
 }
--- JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/business/gymBuissness.java
+++ JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/business/gymBuissness.java
@@ -5,7 +5,7 @@ import com.flipfit.business.Interface.GymCentre;
 
 public class gymBuissness implements GymCentre {
     @Override
-    public Gym updateGymCentre(int gymId, String gymName, int capacity, boolean listed, int userId) {
+    public void updateGymCentre(int gymId, String gymName, int capacity, boolean listed, int userId) {
         Gym gymCentre = new Gym();
         gymCentre.setGymName(gymName);
         gymCentre.setGymId(gymId);
