6013aeff164dc786b7f6a855dff1b9865e49aceb
arpit.shrivastava <f20190818@pilani.bits-pilani.ac.in>
2024-10-10 10:58:53 +0530

    Updated FlipFit Admin Menu

--- JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/client/FlipfitAdminMenu.java
+++ JEDI-Flipfit-Java-POS/src/main/java/com/flipfit/client/FlipfitAdminMenu.java
@@ -1,8 +1,31 @@
 package com.flipfit.client;
 
+import java.util.Scanner;
+
 public class FlipfitAdminMenu {
     public static void display() {
         System.out.println("Welcome to Admin Menu");
+        System.out.println("1. View pending requests");
+        System.out.println("2. Go back to previous menu");
+
+
+        Scanner sc=new Scanner(System.in);
+        int choice=sc.nextInt();
+
+        switch (choice){
+            case 1: {
+                //TODO - print pending requests
+                System.out.println("Enter owner id to be approved");
+                int ownerId = sc.nextInt();
+                //TODO - Approve request and Gym to database
+            }
+            case 2:{
+                //TODO - Go back to previous menu
+            }
+            default:{
+                //TODO- INVALID INPUT
+            }
+        }
         // Add admin-specific options here
     }
 }
